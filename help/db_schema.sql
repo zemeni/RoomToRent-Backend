d0368a835b8fb02ed252c42e3d27d8f4b55be858
Babu <neupanebabu828@gmail.com>
2024-08-04 23:36:00 +0930

    db schema updated

--- help/db_schema.sql
+++ help/db_schema.sql
@@ -12,84 +12,45 @@ CREATE TABLE users(
 
 insert into users (firstname, lastname, email, password, phone) VALUES ('a','b','ddddd','p','0uu');
 
-CREATE TABLE propertyTypes(
-                              PropertyTypeId SERIAL PRIMARY KEY ,
-                              TypeName VARCHAR(50) NOT NULL
+
+CREATE TABLE units (
+                       id SERIAL PRIMARY KEY,
+                       address VARCHAR(255),
+                       price DECIMAL,
+                       bondPrice DECIMAL,
+                       rooms INTEGER,
+                       description TEXT,
+                       bathrooms INTEGER,
+                       parkings INTEGER,
+                       startDate TIMESTAMP,
+                       endDate TIMESTAMP,
+                       phone1 VARCHAR(20),
+                       phone2 VARCHAR(20),
+                       latitude DOUBLE PRECISION,
+                       longitude DOUBLE PRECISION,
+                       images TEXT[],
+                       userID INTEGER,
+                       unitNumber INTEGER
 );
 
-INSERT INTO propertyTypes (TypeName) VALUES ('House'), ('Townhouse'), ('Apartment');
-
-CREATE TABLE properties(
-  PropertyId SERIAL PRIMARY KEY,
-  OwnerId INT REFERENCES users(UserId),
-  PropertyTypeId INT REFERENCES propertyTypes(propertytypeid),
-  AddressId INT REFERENCES address(addressid),
-  Description TEXT,
-  NumberOfRooms INT,
-  NumberOfBathroom INT,
-  NumberOfParking INT,
-  Price DECIMAL(10,2),
-  PricePeriod VARCHAR(50),
-  AvailableFrom DATE,
-  AvailableTo DATE
+CREATE TABLE rooms (
+                       id SERIAL PRIMARY KEY,
+                       address VARCHAR(255),
+                       price DECIMAL,
+                       including BOOLEAN,
+                       roomType VARCHAR(50),
+                       furnished BOOLEAN,
+                       description TEXT,
+                       bathrooms INTEGER,
+                       parkings INTEGER,
+                       startDate TIMESTAMP,
+                       endDate TIMESTAMP,
+                       images TEXT[],
+                       userId INTEGER,
+                       roomNumber INTEGER,
+                       latitude DOUBLE PRECISION,
+                       longitude DOUBLE PRECISION,
+                       gender VARCHAR(20),
+                       phone1 VARCHAR(20),
+                       phone2 VARCHAR(20)
 );
-
-CREATE TABLE rooms(
-    RoomId SERIAL PRIMARY KEY,
-    PropertyId INT REFERENCES properties(PropertyId),
-    OwnerId INT REFERENCES users(userid),
-    AddressId INT REFERENCES address(addressid),
-    RoomNumber INTEGER,
-    Furnished BOOLEAN,
-    RoomTypeId INT REFERENCES roomType(RoomTypeId),
-    Price DECIMAL(10,2),
-    PricePeriod VARCHAR(50),
-    AvailableFrom DATE,
-    AvailableTo DATE,
-    Description TEXT
-);
-
-CREATE TABLE  roomType(
-  RoomTypeId SERIAL PRIMARY KEY,
-  RoomType VARCHAR(15) -- SINGLE, DOUBLE
-);
-
-insert into users values (1, 'Baburam', 'Neupane', 'neupanebabu828@gmail.com', 'abc123', '0452445***',1);
-insert into users values (2, 'Subodh', 'Tiwari', 'subodh.tiwari@gmail.com', 'abc123', '0452445***',1);
-insert into users values (3, 'Gaurav', 'Neupane', 'gaurav.neupane@gmail.com', 'abc123', '0452445***',2);
-insert into users values (4, 'Roman', 'Paudel', 'roman.paudel@gmail.com', 'abc123', '0452445***',2);
-
-select * from users;
-
-insert into properties (ownerid, description, numberofrooms, numberofbathroom, numberofparking, price, availablefrom, availableto) values
-                       (1, 'very good 1 room available', 3, 1, 1, 0, '2025-04-29', '2029-04-30');
-insert into properties (ownerid, description, numberofrooms, numberofbathroom, numberofparking, price, availablefrom, availableto) values
-                       (2, 'very good 1 room available', 3, 1, 1, 0, '2025-04-29', '2029-04-30');
-insert into properties (ownerid, description, numberofrooms, numberofbathroom, numberofparking, price, availablefrom, availableto) values
-                       (3, 'very good 1 room available', 3, 1, 1, 0, '2025-04-29', '2029-04-30');
-select * from properties;
-
-
-insert into rooms(propertyid, roomnumber, furnished, roomtypeid, price, availablefrom, availableto, description) values
-                  (1, 1, true, 1, 320, '2024-05-24', '2029-05-09', 'furnished single room available in marion');
-insert into rooms(propertyid, roomnumber, furnished, roomtypeid, price, availablefrom, availableto, description) values
-                  (1, 2, true, 1, 230, '2024-05-24', '2029-05-09', 'furnished single room available in marion');
-insert into rooms(propertyid, roomnumber, furnished, roomtypeid, price, availablefrom, availableto, description) values
-                  (2, 1, true, 1, 320, '2024-05-24', '2029-05-09', 'furnished single room available in marion');
-insert into rooms(propertyid, roomnumber, furnished, roomtypeid, price, availablefrom, availableto, description) values
-                  (2, 2, true, 1, 320, '2024-05-24', '2029-05-09', 'furnished single room available in marion');
-
-
-CREATE TABLE address(
-                        AddressId SERIAL PRIMARY KEY,
-                        FullAddress VARCHAR(255) NOT NULL,
-                        Suburb VARCHAR(100) NOT NULL ,
-                        State VARCHAR(3) NOT NULL ,
-                        Postcode INTEGER NOT NULL ,
-                        Country VARCHAR(50) NOT NULL
-);
-
-alter table address
-alter column postcode type int;
-
-select * from address;
